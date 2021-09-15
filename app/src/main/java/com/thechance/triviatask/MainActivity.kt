9c123618330496841302d4c4d8c69b1175fa3009
Rafal A <rafal.akeel99@gmail.com>
2021-09-15 04:54:31 +0300

    bind the data to the layout and display information

--- app/src/main/java/com/thechance/triviatask/MainActivity.kt
+++ app/src/main/java/com/thechance/triviatask/MainActivity.kt
@@ -2,10 +2,18 @@ package com.thechance.triviatask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thechance.triviatask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding :ActivityMainBinding
+    private  val fragmentQuestion = questionFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+    }
+    private fun initView() {
+        supportFragmentManager.beginTransaction().add(R.id.container,fragmentQuestion).commit()
     }
 }
--- app/src/main/java/com/thechance/triviatask/questionFragment.kt
+++ app/src/main/java/com/thechance/triviatask/questionFragment.kt
@@ -1,21 +1,95 @@
 package com.thechance.triviatask
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import com.google.gson.Gson
 import com.thechance.triviatask.databinding.FragmentQuestionsBinding
+import okhttp3.*
+import java.io.IOException
 
 
 class questionFragment:BaseFragment<FragmentQuestionsBinding>() {
+    val client = OkHttpClient()
     override val LOG_TAG: String
         get() = javaClass.simpleName
     override val bindingInflater: (LayoutInflater) -> FragmentQuestionsBinding = FragmentQuestionsBinding::inflate
 
     override fun setup() {
-        TODO("Not yet implemented")
+        showInfo()
+        getNextQuestion()
+        getCorrectAnswer()
     }
 
     override fun addCallBack() {
-        TODO("Not yet implemented")
     }
+
+
+    private fun showInfo() {
+            val url = "https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple"
+            val request = Request.Builder().url(url).build()
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    println("FAILED , ${e.message.toString()}")
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val body = response.body?.string().let { jsonString ->
+                        val homeInfo = Gson().fromJson(jsonString, TriviaQuestion::class.java)
+                        val info = homeInfo.results?.toMutableList()?.get(0)
+                        activity?.runOnUiThread(Runnable {
+                            binding?.textQuestion?.text = info?.question
+                            binding?.textFirstAnswer?.text = info?.correct_answer
+                            binding?.textSecondAnswer?.text = info?.incorrect_answers?.get(0)
+                            binding?.textThirdAnswer?.text = info?.incorrect_answers?.get(1)
+                            binding?.textFourthAnswer?.text = info?.incorrect_answers?.get(2)
+                        })
+                    }
+
+
+
+                }
+
+            })
+    }
+
+    private fun getNextQuestion() {
+
+        binding?.buttonNext?.setOnClickListener {
+            showInfo()
+            getDefaultStyle()
+        }
+    }
+
+    @SuppressLint("ResourceType")
+    private fun getCorrectAnswer() {
+        binding?.textFirstAnswer?.setOnClickListener {
+            getAnswer()
+        }
+        binding?.textSecondAnswer?.setOnClickListener {
+            getAnswer()
+        }
+        binding?.textThirdAnswer?.setOnClickListener {
+            getAnswer()
+        }
+        binding?.textFourthAnswer?.setOnClickListener {
+            getAnswer()
+        }
+    }
+
+        private fun getAnswer() {
+            binding?.textFirstAnswer?.setBackgroundResource(R.drawable.correct_answer)
+            binding?.textSecondAnswer?.setBackgroundResource(R.drawable.wrong_answer)
+            binding?.textThirdAnswer?.setBackgroundResource(R.drawable.wrong_answer)
+            binding?.textFourthAnswer?.setBackgroundResource(R.drawable.wrong_answer)
+
+
+        }
+        private fun getDefaultStyle(){
+            binding?.textFirstAnswer?.setBackgroundResource(R.drawable.text_background)
+            binding?.textSecondAnswer?.setBackgroundResource(R.drawable.text_background)
+            binding?.textThirdAnswer?.setBackgroundResource(R.drawable.text_background)
+            binding?.textFourthAnswer?.setBackgroundResource(R.drawable.text_background)
+        }
 
 
 }
