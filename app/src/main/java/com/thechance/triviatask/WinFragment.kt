4a814c3e806f43bc88f1f1ac10dd66633b52f9d8
Rafal A <rafal.akeel99@gmail.com>
2021-09-16 07:05:10 +0300

    display result in win fragment

--- app/src/main/java/com/thechance/triviatask/WinFragment.kt
+++ app/src/main/java/com/thechance/triviatask/WinFragment.kt
@@ -11,38 +11,50 @@ import androidx.fragment.app.Fragment
 import com.thechance.triviatask.databinding.FragmentWinningBinding
 
 class WinFragment:BaseFragment<FragmentWinningBinding>() {
+    var points:Int = 0
     override val LOG_TAG: String
         get()= "win fragment"
-    private val points = arguments?.getInt(Constatnt.POINTS)
 
     override val bindingInflater: (LayoutInflater) -> FragmentWinningBinding = FragmentWinningBinding::inflate
 
 
     override fun setup() {
-        Log.v(LOG_TAG, arguments?.getInt(Constatnt.POINTS).toString())
+        points = requireArguments().getInt(Constatnt.POINTS)
+
+        //println("FROM FRAGMENT : $points")
         showResult()
         binding?.buttonSong?.setOnClickListener{
-            val url = "https://www.youtube.com/watch?v=0aUav1lx3rA"
-            val intent= Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse(url)
-            startActivity(intent)
+           displaySong()
         }
+        binding?.buttonResetGame?.setOnClickListener {
+            restartGame()
+        }
+    }
+
+    private fun restartGame() {
+        val questionFragment = QuestionFragment()
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.container,questionFragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun displaySong() {
+        val url = "https://www.youtube.com/watch?v=0aUav1lx3rA"
+        val intent= Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
     }
 
     private fun showResult() {
-        if (points != null) {
-            if (points >=5){
-                binding?.textResult?.text =Constatnt.WIN_TEXT
-                binding?.textPoints?.text = points.toString()
-            }
-            else{
-                binding?.textResult?.text =Constatnt.LOSE_TEXT
-                binding?.textPoints?.text = points.toString()
-            }
+        if (points > 5){
+            binding?.textResult?.text = Constatnt.WIN_TEXT
+            binding?.textPoints?.text = points.toString()
+        } else{
+            binding?.textResult?.text =Constatnt.LOSE_TEXT
+            binding?.textPoints?.text = points.toString()
         }
-
-
-    }
+        }
 
 
     override fun addCallBack() {
